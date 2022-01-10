92bc9e31a16f279ec9a3e0de2bf3a2843b9b7d4f
Jennifer <jenniferroques@gmail.com>
2022-01-10 11:29:18 -0600

    Updated Styling in main.css

---
+++ main.js
@@ -0,0 +1,57 @@
+window.addEventListener('DOMContentLoaded', (e) => {
+   
+// Declare variables 
+
+
+// Add Event Listeners 
+
+    // Likes 
+
+    // Add Notes 
+
+
+// Standalone Functions
+
+    //Fetch 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
