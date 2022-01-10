1cffd87bdc5b88d3c395b3d1324d8e4c7d3f1242
Jennifer <jenniferroques@gmail.com>
2022-01-10 13:42:36 -0600

    Added villagers.json file

--- main.js
+++ main.js
@@ -27,17 +27,29 @@ window.addEventListener('DOMContentLoaded', (e) => {
 fetch ('https://acnhapi.com/v1a/')
 .then(resp => resp.json())
 .then((villagers) => {
-    villiagers.forEach((villager))
+    villagers.forEach((villager))
 })
 
 
 
 function renderVillager(villager) {
-    let card = document.createElement(li)
-    card 
-    
-    
-    `
+    let card = document.createElement('li')
+    card.className = 'card'
+    card.innerHTML = `
+     <img src="${villager.image_uri}">
+     <div class="content">
+        <h3>${villager.name-USen}</h3>
+        <h4>${villager.personality}
+        <p>${villager.saying}</p> 
+    </div>`
+
+    document.querySelector('#villager-list').append(card)
+}
+
+function render() {
+    villagers.forEach(villager => renderVillager(villager))
+    }
+    render()
 }
 
     //Search through json? 
