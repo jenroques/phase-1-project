e6b0559b0cb79be63a095bd0d3128d92d36eb0fd
Jennifer <jenniferroques@gmail.com>
2022-01-10 13:10:45 -0600

    Added Villager Card UL to HTML

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 window.addEventListener('DOMContentLoaded', (e) => {
-   const villagerContainer = document.getElementById('villager-container')
-   
+    const villagerContainer = document.getElementById('villager-container')
+    const fullHeart = '♥'
+    const emptyHeart = '♡'
+    const backButton = document.getElementById('back')
+    const forwardButton = document.getElementById('forward')
+    let pageNum = 1
+
+
 // Declare variables 
 
 
@@ -8,12 +14,33 @@ window.addEventListener('DOMContentLoaded', (e) => {
 
     // Likes 
 
-    // Add Notes 
+    // Add Notes ????
+
+    // Favorite Button 
+
+
 
 
 // Standalone Functions
 
     //Fetch 
+fetch ('https://acnhapi.com/v1a/')
+.then(resp => resp.json())
+.then((villagers) => {
+    villiagers.forEach((villager))
+})
+
+
+
+function renderVillager(villager) {
+    let card = document.createElement(li)
+    card 
+    
+    
+    `
+}
+
+    //Search through json? 
 
 
 
