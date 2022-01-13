680bea5c35fefaa00e336bfdaaa26cfd891ce54d
Jennifer <jenniferroques@gmail.com>
2022-01-13 08:00:41 -0600

    Added fetch helper functions

--- main2.js
+++ main2.js
@@ -2,22 +2,39 @@
 window.addEventListener('DOMContentLoaded', (e) => {
 
     const villagerContainer = document.getElementById('villager-list');
+    const forwardButton = document.getElementById('forward');
+    const backButton = document.getElementById('back');
 
+    let currentPage = 1
 
 // Event Listeners 
 
+forwardButton.addEventListener("click", () => {
+    console.log("Clicky!")
+
+
+
+})
+
+
+backButton.addEventListener("click", () => {
+    console.log("Back Click!")
+})
 
 
 
 
+//Fetch Functions 
 
-
+function getAllVillagers(page) {
+    fetch(`http://localhost:3000/villagers/?_limit=5&_page=${page}`)
+    .then(res => res.json())
+    .then(renderAllVillagers)
+}
 
 
 //Initial Fetch & Render 
-fetch("http://localhost:3000/villagers/?_limit=5&_page=1")
-    .then(res => res.json())
-    .then(renderAllVillagers)
+getAllVillagers(currentPage)
 
 //Render Helpers
 
@@ -33,11 +50,6 @@ function renderVillager(villager) {
         <img src="${villager.image_uri}">
         <h3>${villager["name"]["name-USen"]}</h3>
         <h4>${villager.saying}</h4>
-        <footer>
-        <ul>
-          <li class="like">Like! <span class="like-glyph">&#x2661;</span></li>
-        </ul>
-        </footer>
         `
 
   villagerContainer.append(villagerDiv)
