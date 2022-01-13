a0c91f7c7727b6669dc74f8d1427984d0e0162e0
Jennifer <jenniferroques@gmail.com>
2022-01-13 05:48:56 -0600

    added new back and forward buttons

--- main2.js
+++ main2.js
@@ -7,13 +7,32 @@ window.addEventListener('DOMContentLoaded', (e) => {
 // Event Listeners 
 
 
+
+
+
+
+
+
+
+//Initial Fetch & Render 
+fetch("http://localhost:3000/villagers/?_limit=5&_page=1")
+    .then(res => res.json())
+    .then(renderAllVillagers)
+
 //Render Helpers
+
+function renderAllVillagers(villagers) {
+    villagers.forEach(renderVillager)
+}
+
+
 function renderVillager(villager) {
+    console.log(villager)
     const villagerDiv = document.createElement("div")
     villagerDiv.innerHTML = `
-        <img src="${villagers.image_uri}">
-        <h3>${villagers["name"]["name-USen"]}</h3>
-        <h4>${villagers.saying}</h4>
+        <img src="${villager.image_uri}">
+        <h3>${villager["name"]["name-USen"]}</h3>
+        <h4>${villager.saying}</h4>
         <footer>
         <ul>
           <li class="like">Like! <span class="like-glyph">&#x2661;</span></li>
@@ -23,17 +42,5 @@ function renderVillager(villager) {
 
   villagerContainer.append(villagerDiv)
 }
-
-function renderAllVillagers(villagers) {
-    villagers.forEach(renderVillager)
-}
-
-
-
-//Initial Fetch & Render 
-fetch("http://localhost:3000/villagers/?_limit=5&_page=1")
-    .then(res => res.json())
-    .then(renderAllVillagers)
-
 
 });
